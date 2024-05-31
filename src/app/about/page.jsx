9082a1fc94f5b4bfffb65d37f95e17a01f2f4354
Masoud61UI <masoud6181@gmail.com>
2024-05-31 22:50:19 +0330

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,17 @@
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, excepturi dolorum consectetur molestiae quasi at quod tenetur.
+            quis beatae corporis minima exercitationem maxime, mollitia, eveniet eum et maiores itaque velit!
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi illum consectetur doloribus quis tempore ipsa accusamus quia. Facere omnis dolorum corrupti tenetur placeat modi officia natus suscipit voluptatem beatae, nisi blanditiis velit non itaque fugiat accusamus ipsam cum consectetur esse. Nihil pariatur non eius possimus sit, sequi, exercitationem fugiat consectetur dolorum ad maiores explicabo est quaerat accusantium. Deleniti suscipit in cum quas quo officia eos, saepe asperiores tempore nulla alias incidunt sequi aspernatur, id perferendis ab dicta accusamus temporibus earum?
+        </p>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, fugit incidunt minus esse dolorem reiciendis quia pariatur aliquam quasi, odit possimus reprehenderit dicta a. Nesciunt deserunt dicta minus pariatur illo, iste voluptatum fugiat quo illum sint, sequi eveniet animi? Itaque, ea laboriosam. Fugit aspernatur quaerat eum dolorem saepe!
+        </p>
+    </div>
+  )
+}
