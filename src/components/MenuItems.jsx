14a0f9fbf9a0ae2924f0385c9dcda8e51085b0d4
Masoud61UI <masoud6181@gmail.com>
2024-05-31 21:33:02 +0330

    Completion of header section edits

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function MenuItems({ title, address, Icon }) {
   return (
-    <Link href={address} className="hover:text-amber-500">
+    <Link href={address} className="transition duration-180 ease-out hover:ease-in hover:text-amber-500">
         <Icon className='text-2xl sm:hidden' />
         <li className="list-none hidden text-sm sm:inline">{title}</li>
     </Link>
