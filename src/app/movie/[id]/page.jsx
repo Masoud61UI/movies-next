7a5808d484d30738bfe2209d73772a943d296365
Masoud61UI <masoud6181@gmail.com>
2024-06-01 18:50:02 +0330

    movie section edits

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,3 +1,6 @@
+import { SlLike } from "react-icons/sl";
+import { BsCalendar2Date } from "react-icons/bs";
+
 export default async function page({ params }) {
   const movieId = params.id;
   const res = await fetch(
@@ -13,19 +16,29 @@ export default async function page({ params }) {
             movie.backdrop_path || movie.poster_path
           }`}
           alt="Movie Image"
-          className="rounded-lg w-full h-[300px]"
+          className="rounded-lg basis-2/6 w-[500px] h-[300px]"
         />
-        <div className="p-3">
-            <h2 className="text-2xl mb-4 font-bold">{movie.title || movie.name}</h2>
-            <p className="text-lg mb-4">{movie.overview}</p>
-            <p className="mb-4">
-                <span className="font-semibold mr-2">Date Released:</span>
-                {movie.release_date || movie.first_air_date}
-            </p>
-            <p className="mb-4">
-                <span className="font-semibold mr-2">Rating:</span>
-                {movie.vote_count}
-            </p>
+        <div className="p-3 basis-4/6 w-full">
+            <h2 className="text-3xl mb-5 font-bold">{movie.title || movie.name}</h2>
+            <p className="text-lg mb-6">{movie.overview}</p>
+            <div className="mb-4 flex items-center">
+                <div className="flex items-center">
+                  <BsCalendar2Date className="h-5 mr-2" />
+                  <span className="font-semibold mr-2">Date Released:</span>
+                </div>
+                <p>
+                  {movie.release_date || movie.first_air_date}
+                </p>
+            </div>
+            <div className="mb-4 flex items-center">
+                <div className="flex items-center">
+                  <SlLike className="h-5 mr-2" />
+                  <span className="font-semibold mr-2">Rating:</span>
+                </div>
+                <p>
+                  {movie.vote_count}
+                </p>
+            </div>
         </div>
       </div>
     </div>
